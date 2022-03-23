b10aa83c0c0bdc9b5db1bb69be219664da2f9536
Neil Peyssard <neilpeyssard44@gmail.com>
2022-03-23 09:57:17 +0100

    Fix constraints in configuration form

--- src/Form/Type/MondialRelayRangeType.php
+++ src/Form/Type/MondialRelayRangeType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MondialRelayRangeType extends AbstractType
@@ -22,14 +22,14 @@ class MondialRelayRangeType extends AbstractType
                 'label' => 'sylius.form.shipping_calculator.mondial_relay.min_weight',
                 'required' => false,
                 'constraints' => [
-                    new GreaterThan(['value' => 0, 'groups' => ['sylius']]),
+                    new GreaterThanOrEqual(['value' => 0, 'groups' => ['sylius']]),
                 ]
             ])
             ->add('maxWeight', IntegerType::class, [
                 'label' => 'sylius.form.shipping_calculator.mondial_relay.max_weight',
                 'required' => false,
                 'constraints' => [
-                    new GreaterThan(['value' => 0, 'groups' => ['sylius']]),
+                    new GreaterThanOrEqual(['value' => 0, 'groups' => ['sylius']]),
                 ],
             ])
             ->add('amount', MoneyType::class, [
@@ -37,7 +37,7 @@ class MondialRelayRangeType extends AbstractType
                 'currency' => $options['currency'],
                 'constraints' => [
                     new NotBlank(['groups' => ['sylius']]),
-                    new GreaterThan(['value' => 0, 'groups' => ['sylius']]),
+                    new GreaterThanOrEqual(['value' => 0, 'groups' => ['sylius']]),
                 ]
             ])
         ;
