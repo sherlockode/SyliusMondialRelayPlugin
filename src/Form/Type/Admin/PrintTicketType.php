f239ba7d95d0cad9a6b649ecc2422b37d5188c65
Neil Peyssard <neil@sherlockode.fr>
2023-02-15 09:56:52 +0100

    Add custome modes for parcels delivery and collection

--- src/Form/Type/Admin/PrintTicketType.php
+++ src/Form/Type/Admin/PrintTicketType.php
@@ -3,6 +3,7 @@
 namespace Sherlockode\SyliusMondialRelayPlugin\Form\Type\Admin;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
@@ -16,6 +17,30 @@ class PrintTicketType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('collectionMode', ChoiceType::class, [
+                'label' => 'sylius.mondial_relay.collection_mode',
+                'constraints' => [new NotBlank()],
+                'placeholder' => 'sylius.mondial_relay.please_select_a_value',
+                'choices' => [
+                    'sylius.mondial_relay.collection_modes.rel' => 'REL',
+                    'sylius.mondial_relay.collection_modes.ccc' => 'CCC',
+                    'sylius.mondial_relay.collection_modes.cdr' => 'CDR',
+                    'sylius.mondial_relay.collection_modes.cds' => 'CDS',
+                ],
+            ])
+            ->add('deliveryMode', ChoiceType::class, [
+                'label' => 'sylius.mondial_relay.delivery_mode',
+                'constraints' => [new NotBlank()],
+                'placeholder' => 'sylius.mondial_relay.please_select_a_value',
+                'choices' => [
+                    'sylius.mondial_relay.delivery_modes.24r' => '24R',
+                    'sylius.mondial_relay.delivery_modes.24l' => '24L',
+                    'sylius.mondial_relay.delivery_modes.hom' => 'HOM',
+                    'sylius.mondial_relay.delivery_modes.ld1' => 'LD1',
+                    'sylius.mondial_relay.delivery_modes.lds' => 'LDS',
+                    'sylius.mondial_relay.delivery_modes.lcc' => 'LCC',
+                ],
+            ])
             ->add('parcelCount', IntegerType::class, [
                 'label' => 'sylius.mondial_relay.parcel_count',
                 'constraints' => [new NotBlank(), new GreaterThanOrEqual(['value' => 1])],
